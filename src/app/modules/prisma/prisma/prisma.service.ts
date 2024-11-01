dd62123fb8b540598e5aa97237585be5d33d313e
FairyPenguin <mahmoud.abd-elaziz@teqneyat.com>
2024-11-01 23:59:44 +0200

    Update docker-compose file to catch the env variable for the DATABASE_URL which required by prisma

--- src/app/modules/prisma/prisma/prisma.service.ts
+++ src/app/modules/prisma/prisma/prisma.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { dataBaseInit } from '../url';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     onModuleInit() {
+
+        dataBaseInit()
+
         this.$connect()
             .then(() => console.log("Database Connected Successfully 🎉 🎊"))
             .catch((error) => console.log("❌ Error, failed connecting to the DB: \n \n", error))
---
+++ src/app/modules/prisma/url.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+
+function getDatabaseURL(): string {
+
+    const password = fs.readFileSync('/run/secrets/db-password', 'utf8').trim();
+
+    // postgresql://postgres:11@localhost:5432/nestjsdatabase?schema=public
+
+    const URL = `postgresql://${process.env.POSTGRES_USER}:${password}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}?sslmode=${process.env.POSTGRES_SSLMODE}`
+
+    return URL
+}
+
+
+export function dataBaseInit() {
+    // Read the database password from the secret file
+    const dbPassword = fs.readFileSync('/run/secrets/database-password', 'utf8').trim();
+    process.env.DATABASE_URL = `postgresql://postgres:${dbPassword}@database:5432/nestdatabase`;
+
+    // Now Prisma will use this DATABASE_URL when it initializes
+}
