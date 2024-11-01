40dfac0d9f73d269a77cfdb044ae2262e0f80712
FairyPenguin <mahmoud.abd-elaziz@teqneyat.com>
2024-11-02 01:03:49 +0200

    Add migrations

--- deploy.sh
+++ deploy.sh
@@ -49,7 +49,7 @@ docker ps -q -f name=nest-backend | wc -l
 
 sleep 10
 # docker-compose exec -T nest-backend pnpx prisma migrate deploy
-docker-compose exec -T nest-backend sh -c 'export DATABASE_URL=postgresql://postgres:$(cat /run/secrets/database-password)@db:5432/nestdatabase && pnpx prisma migrate deploy'
+docker-compose exec -T nest-backend sh -c 'export DATABASE_URL=postgresql://postgres:$(cat /run/secrets/database-password)@database:5432/nestdatabase && pnpx prisma migrate deploy'
 
 
 # sshpass -p "$VPS_PASSWORD" ssh -i ~/.ssh/id_rsa admin@49.13.174.222 'bash -s' 
